f1c66b1dd9290e8be851942ae4c58fdadf101267
namrata <namrataap725@gmail.com>
2025-07-17 21:08:23 +0545

    Fix error by validating data before mapping in Card component

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,8 +1,14 @@
 import "./card.css";
+
 function Card({ data }) {
   const readMore = (url) => {
     window.open(url);
   };
+
+  if (!data || !Array.isArray(data) || data.length === 0) {
+    return <div className="no-articles">No articles to display.</div>;
+  }
+
   return (
     <div className="card-container">
       {data.map((article, index) => {
@@ -11,9 +17,11 @@ function Card({ data }) {
         } else {
           return (
             <div className="card" key={index}>
-              <img src={article.urlToImage} />
+              <img src={article.urlToImage} alt={article.title} />
               <div className="cardContainer">
-                <a>{article.title}</a>
+                <a href={article.url} target="_blank" rel="noopener noreferrer">
+                  {article.title}
+                </a>
                 <p>{article.description}</p>
                 <button onClick={() => readMore(article.url)}>Read More</button>
               </div>
@@ -24,4 +32,5 @@ function Card({ data }) {
     </div>
   );
 }
+
 export default Card;
